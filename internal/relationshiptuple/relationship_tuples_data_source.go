58a29786ce1c5096d85399c082272bc3bccc3ed7
Maurice <m.ackel@icloud.com>
2025-02-12 20:37:40 +0000

    Added relationship tuples data source

---
+++ internal/relationshiptuple/relationship_tuples_data_source.go
@@ -0,0 +1,165 @@
+package relationshiptuple
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	openfga "github.com/openfga/go-sdk"
+	"github.com/openfga/go-sdk/client"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &RelationshipTuplesDataSource{}
+var _ datasource.DataSourceWithConfigure = &RelationshipTuplesDataSource{}
+
+func NewRelationshipTuplesDataSource() datasource.DataSource {
+	return &RelationshipTuplesDataSource{}
+}
+
+type RelationshipTuplesDataSource struct {
+	client *client.OpenFgaClient
+}
+
+type RelationshipTuplesDataSourceModel struct {
+	StoreId            types.String             `tfsdk:"store_id"`
+	Query              *TupleQuery              `tfsdk:"query"`
+	RelationshipTuples []RelationshipTupleModel `tfsdk:"relationship_tuples"`
+}
+
+type TupleQuery struct {
+	User     types.String `tfsdk:"user"`
+	Relation types.String `tfsdk:"relation"`
+	Object   types.String `tfsdk:"object"`
+}
+
+func (d *RelationshipTuplesDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_relationship_tuples"
+}
+
+func (d *RelationshipTuplesDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "A relationship tuple is a tuple consisting of a user, relation, and object. Tuples may add an optional condition.",
+
+		Attributes: map[string]schema.Attribute{
+			"store_id": schema.StringAttribute{
+				MarkdownDescription: "The unique ID of the OpenFGA store this relationship tuple belongs to",
+				Required:            true,
+			},
+			"query": schema.SingleNestedAttribute{
+				MarkdownDescription: "A query to filter the returned tuples (leave empty to read all tuples)",
+				Optional:            true,
+				Attributes: map[string]schema.Attribute{
+					"user": schema.StringAttribute{
+						MarkdownDescription: "The user of the OpenFGA relationship tuple",
+						Optional:            true,
+					},
+					"relation": schema.StringAttribute{
+						MarkdownDescription: "The relation of the OpenFGA relationship tuple",
+						Optional:            true,
+					},
+					"object": schema.StringAttribute{
+						MarkdownDescription: "The object of the OpenFGA relationship tuple",
+						Required:            true,
+					},
+				},
+			},
+			"relationship_tuples": schema.ListNestedAttribute{
+				MarkdownDescription: "List of OpenFGA relationship tuples",
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"store_id": schema.StringAttribute{
+							MarkdownDescription: "The unique ID of the OpenFGA store this relationship tuple belongs to",
+							Computed:            true,
+						},
+						"user": schema.StringAttribute{
+							MarkdownDescription: "The user of the OpenFGA relationship tuple",
+							Computed:            true,
+						},
+						"relation": schema.StringAttribute{
+							MarkdownDescription: "The relation of the OpenFGA relationship tuple",
+							Computed:            true,
+						},
+						"object": schema.StringAttribute{
+							MarkdownDescription: "The object of the OpenFGA relationship tuple",
+							Computed:            true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *RelationshipTuplesDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.OpenFgaClient)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *client.OpenFgaClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *RelationshipTuplesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state RelationshipTuplesDataSourceModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var tuples []openfga.Tuple
+	options := client.ClientReadOptions{
+		StoreId:           state.StoreId.ValueStringPointer(),
+		ContinuationToken: openfga.PtrString(""),
+	}
+
+	var body client.ClientReadRequest
+	if state.Query != nil {
+		body = client.ClientReadRequest{
+			User:     state.Query.User.ValueStringPointer(),
+			Relation: state.Query.Relation.ValueStringPointer(),
+			Object:   state.Query.Object.ValueStringPointer(),
+		}
+	}
+
+	for isLastPage := false; !isLastPage; isLastPage = *options.ContinuationToken == "" {
+		response, err := d.client.Read(ctx).Options(options).Body(body).Execute()
+		if err != nil {
+			resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read relationship tuples, got error: %s", err))
+			return
+		}
+
+		tuples = append(tuples, response.Tuples...)
+
+		options.ContinuationToken = openfga.PtrString(response.ContinuationToken)
+	}
+
+	state.RelationshipTuples = []RelationshipTupleModel{}
+	for _, tuple := range tuples {
+		state.RelationshipTuples = append(state.RelationshipTuples, RelationshipTupleModel{
+			StoreId:  types.StringValue(*options.StoreId),
+			User:     types.StringValue(tuple.Key.User),
+			Relation: types.StringValue(tuple.Key.Relation),
+			Object:   types.StringValue(tuple.Key.Object),
+		})
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
